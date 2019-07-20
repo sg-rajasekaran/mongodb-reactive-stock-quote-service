5975100a8efadb516e841e3ea86a0f050f27e994
natrajanrajasekaran <natrajan.rajasekaran@icloud.com>
2019-07-20 18:55:54 +0800

    Created Initial project - mongodb-reactive-stock-quote-service

---
+++ mongodb-reactive-stock-quote-service/src/main/java/sg/reactivespring/mongodbreactivestockquoteservice/MongodbReactiveStockQuoteServiceApplication.java
@@ -0,0 +1,13 @@
+package sg.reactivespring.mongodbreactivestockquoteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbReactiveStockQuoteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbReactiveStockQuoteServiceApplication.class, args);
+    }
+
+}
---
+++ mongodb-reactive-stock-quote-service/src/test/java/sg/reactivespring/mongodbreactivestockquoteservice/MongodbReactiveStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.reactivespring.mongodbreactivestockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbReactiveStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
